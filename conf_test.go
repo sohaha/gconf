78766e61ae299a9e6843e526a9de55d560051ef6
seekwe <seekwe@gmail.com>
2021-04-03 22:59:47 +0800

    test: fix package name

--- conf_test.go
+++ conf_test.go
@@ -1,24 +1,27 @@
 package gconf_test
 
 import (
-	"github.com/sohaha/go_conf"
+	"github.com/sohaha/gconf"
 	"github.com/sohaha/zlsgo"
-	"os"
+	//"os"
 	"testing"
 )
 
 func TestDef(t *testing.T) {
 	tt := zlsgo.NewTest(t)
 
-	c := go_conf.New("zls")
+	c := gconf.New("zls.json")
 	c.SetDefault("def", 1)
+	c.Set("arr", []struct{ Name string }{{"1"}, {"2"}, {"go"}})
 	err := c.Read()
 	tt.Equal(true, err == nil)
 
-	c2 := go_conf.New("zls.yaml")
+	c2 := gconf.New("zls.toml")
 	err = c2.Read()
+	t.Log(c.GetAll())
+	t.Log(c2.GetAll())
 	tt.Equal(true, err == nil)
 	tt.Equal(c.Core.GetInt("def"), c2.Core.GetInt("def"))
 
-	os.Remove("zls.toml")
+	//os.Remove("zls.toml")
 }
